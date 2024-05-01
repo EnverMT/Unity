9de08f42ce48adafb7009064af76453bae1a3818
Enver Toleuov <anvarmtg@gmail.com>
2024-05-01 11:51:00 +0500

    move and rotate

--- 01.Scripting - Mover/Assets/Scripts/Mover.cs
+++ 01.Scripting - Mover/Assets/Scripts/Mover.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private Vector3 speed;
-    private void Start()
+    private const string Horizontal = nameof(Horizontal);
+    private const string Vertical = nameof(Vertical);
+
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float moveSpeed;
+
+    private void Update()
     {
-        speed = new Vector3(0, 0, 0.01f);
+        Rotate();
+        Move();
     }
 
-    // Update is called once per frame
-    private void Update()
-    {        
-        transform.Translate(speed, Space.Self);
+    private void Rotate()
+    {
+        float rotation = Input.GetAxis(Horizontal);
+
+        transform.Rotate(rotation * rotateSpeed * Vector3.up * Time.deltaTime);
+    }
+
+    private void Move()
+    {
+        float direction = Input.GetAxis(Vertical);
+        float distance = direction * moveSpeed * Time.deltaTime;
+
+        transform.Translate(distance * Vector3.forward);
     }
 }
