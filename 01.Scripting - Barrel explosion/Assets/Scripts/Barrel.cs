d9eac5268d93a569704b87f652ca71be3b491915
EnverMT <anvarmtg@gmail.com>
2024-05-01 15:51:24 +0500

    explosion on mouse clich

--- 01.Scripting - Barrel explosion/Assets/Scripts/Barrel.cs
+++ 01.Scripting - Barrel explosion/Assets/Scripts/Barrel.cs
@@ -1,18 +1,38 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float _explosionRadius;
+    [SerializeField] private float _explosionForce;
+    [SerializeField] private ParticleSystem _effect;
+
+    private void OnMouseUpAsButton()
     {
-        
+        Explode();
+        if (_effect) Instantiate(_effect, transform.position, transform.rotation);
+        Destroy(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Explode()
     {
-        
+        foreach (Rigidbody explodableObjects in GetExplodableObjects())
+        {
+            explodableObjects.AddExplosionForce(_explosionForce, transform.position, _explosionRadius);
+        }
+    }
+
+    private List<Rigidbody> GetExplodableObjects()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, _explosionRadius);
+
+        List<Rigidbody> barrels = new();
+
+        foreach (Collider collider in colliders)
+        {
+            if (collider.attachedRigidbody != null) barrels.Add(collider.attachedRigidbody);
+        }
+
+        return barrels;
     }
 }
