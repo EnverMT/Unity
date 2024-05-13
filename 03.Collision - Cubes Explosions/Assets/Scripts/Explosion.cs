a01fa077b13fc4ef3e363681e1bfff2413b77162
EnverMT <anvarmtg@gmail.com>
2024-05-13 16:23:18 +0500

    renamed

--- 03.Collision - Cubes Explosions/Assets/Scripts/Explosion.cs
+++ 03.Collision - Cubes Explosions/Assets/Scripts/Explosion.cs
@@ -1,13 +1,14 @@
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody))]
 public class Explosion : MonoBehaviour
 {
     private const float _explodeForce = 600;
     private const float _explodeRange = 60;
 
-    public void Explode(GameObject[] childObjects)
+    public void Explode(GameObject[] affectedObjects)
     {
-        foreach (GameObject child in childObjects)
+        foreach (GameObject child in affectedObjects)
         {
             child.GetComponent<Rigidbody>().AddExplosionForce(_explodeForce, transform.position, _explodeRange);
         }
