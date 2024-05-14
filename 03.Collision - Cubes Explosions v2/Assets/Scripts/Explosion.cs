26917c7a41f11227d450a952d9aa79cd6451c7ad
EnverMT <anvarmtg@gmail.com>
2024-05-14 10:49:35 +0500

    removed excess variables

--- 03.Collision - Cubes Explosions v2/Assets/Scripts/Explosion.cs
+++ 03.Collision - Cubes Explosions v2/Assets/Scripts/Explosion.cs
@@ -6,13 +6,11 @@ public class Explosion : MonoBehaviour
     private const float _initExplodeForce = 300f;
     private const float _initExplodeRange = 10f;
 
-    private float _explodeForce;
-    private float _explodeRange;
+    private float _explodeMultiplier;
 
     public void Init(float explosionMultiplier = 1f)
     {
-        _explodeForce = _initExplodeForce * explosionMultiplier;
-        _explodeRange = _initExplodeRange * explosionMultiplier;
+        _explodeMultiplier = explosionMultiplier;
     }
 
     public void Explode()
@@ -21,13 +19,13 @@ public class Explosion : MonoBehaviour
         {
             if (child.TryGetComponent<Rigidbody>(out Rigidbody rigid))
             {
-                rigid.AddExplosionForce(_explodeForce, transform.position, _explodeRange);
+                rigid.AddExplosionForce(_initExplodeForce * _explodeMultiplier, transform.position, _initExplodeRange * _explodeMultiplier);
             }
         }
     }
 
     private Collider[] GetAffectedCubeObjects()
     {
-        return Physics.OverlapSphere(gameObject.transform.position, _explodeRange).Where(obj => obj.TryGetComponent<Cube>(out _)).ToArray();
+        return Physics.OverlapSphere(gameObject.transform.position, _initExplodeRange * _explodeMultiplier).Where(obj => obj.TryGetComponent<Cube>(out _)).ToArray();
     }
 }
