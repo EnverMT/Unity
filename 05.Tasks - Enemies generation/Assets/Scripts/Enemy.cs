f9318b2192993cae0d0418cba9429366b999d815
EnverMT <anvarmtg@gmail.com>
2024-05-20 21:38:20 +0500

    v1

--- 05.Tasks - Enemies generation/Assets/Scripts/Enemy.cs
+++ 05.Tasks - Enemies generation/Assets/Scripts/Enemy.cs
@@ -1,9 +1,31 @@
 using UnityEngine;
 using UnityEngine.Pool;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] private float _speed;
+
     private ObjectPool<Enemy> _pool;
+    private Rigidbody _rigidBody;
+    private Vector3 _direction;
+
+    private void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        gameObject.transform.Translate(_direction * _speed * Time.deltaTime);
+    }
+
+    public void Init(Vector3 spawnPosition, Vector3 direction, float speed)
+    {
+        _direction = direction;
+        _speed = speed;
+        gameObject.transform.position = spawnPosition;
+    }
 
     public void SetPool(ObjectPool<Enemy> pool)
     {
--- 05.Tasks - Enemies generation/Assets/Scripts/Spawner.cs
+++ 05.Tasks - Enemies generation/Assets/Scripts/Spawner.cs
@@ -4,17 +4,37 @@ using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
-    [SerializeField] private Enemy _enemy;
-    [SerializeField] private ObjectPool<Enemy> _enemyPool;
+    private const float _spawnRate = 2f;
 
+    [SerializeField] private Enemy _enemy;
+    [SerializeField] private float _enemySpeed;
+    [SerializeField] private Vector3[] _spawnPoints;
+
+    private ObjectPool<Enemy> _enemyPool;
     private Coroutine _spawnCoroutine;
 
+    #region UnityMethods
     private void Awake()
     {
+        _enemyPool = new ObjectPool<Enemy>(OnEnemyCreate, OnEnemyGet, OnEnemyRelease, OnEnemyDestroy);
     }
 
+    private void OnEnable()
+    {
+        _spawnCoroutine = StartCoroutine(SpawnPeriodically(_spawnRate));
+    }
+
+    private void OnDisable()
+    {
+        if (_spawnCoroutine != null)
+            StopCoroutine(_spawnCoroutine);
+
+        _spawnCoroutine = null;
+    }
+    #endregion
+
     #region EnemyObjectPoolMethods 
-    private Enemy CreateEnemy()
+    private Enemy OnEnemyCreate()
     {
         Enemy enemy = Instantiate(_enemy);
         enemy.SetPool(_enemyPool);
@@ -37,7 +57,7 @@ public class Spawner : MonoBehaviour
     }
     #endregion
 
-    private IEnumerator SpawnPeriodically(float delay = 0.5f)
+    private IEnumerator SpawnPeriodically(float delay)
     {
         WaitForSeconds wait = new WaitForSeconds(delay);
 
@@ -50,8 +70,22 @@ public class Spawner : MonoBehaviour
 
     private Enemy Spawn()
     {
+        if (_spawnPoints.Length == 0)
+            throw new UnityException("Spawn points should be assigned");
+
         Enemy enemy = _enemyPool.Get();
+        Vector3 spawnPoint = _spawnPoints[Random.Range(0, _spawnPoints.Length)];
+        Vector3 direction = GetRandomDirection();
+
+        enemy.Init(spawnPoint, direction, _enemySpeed);
         enemy.transform.SetParent(gameObject.transform, false);
+
         return enemy;
+    }
+
+    private Vector3 GetRandomDirection()
+    {
+        Vector2 vector2 = Random.insideUnitCircle.normalized;
+        return new Vector3(vector2.x, 0, vector2.y);
     }
 }
