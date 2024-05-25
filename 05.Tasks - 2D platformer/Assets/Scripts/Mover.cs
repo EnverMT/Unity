992c437d79dcd76c7d55a2e5dc8ea8d88ad9485e
EnverMT <anvarmtg@gmail.com>
2024-05-25 23:47:43 +0500

    v1mover

--- 05.Tasks - 2D platformer/Assets/Scripts/Mover.cs
+++ 05.Tasks - 2D platformer/Assets/Scripts/Mover.cs
@@ -1,5 +1,41 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Mover : MonoBehaviour
 {
+    [Header("Movement")]
+    [SerializeField] private KeyCode _leftKey;
+    [SerializeField] private KeyCode _rightKey;
+    [SerializeField] private float _speed;
+
+    [Header("Jump")]
+    [SerializeField] private KeyCode _jumpKey;
+    [SerializeField] private float _jumpSpeed;
+    [SerializeField] private bool _isGrounded;
+
+    private Rigidbody2D _body;
+
+    private void Awake()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        _isGrounded = true;
+    }
+
+    private void Update()
+    {
+        if (_isGrounded && Input.GetKey(_jumpKey))
+        {
+            _body.velocity = Vector3.up * _jumpSpeed;
+            _isGrounded = false;
+        }
+
+        if (Input.GetKey(_rightKey))
+            transform.Translate(Vector3.right * _speed * Time.deltaTime);
+
+        if (Input.GetKey(_leftKey))
+            transform.Translate(Vector3.left * _speed * Time.deltaTime);
+    }
 }
