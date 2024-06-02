c20037557fbd5651b7f8596ca5c060011424950e
EnverMT <anvarmtg@gmail.com>
2024-06-02 12:52:34 +0500

    coins spawn

--- 05.Tasks - 2D platformer/Assets/Scripts/Coin.cs
+++ 05.Tasks - 2D platformer/Assets/Scripts/Coin.cs
@@ -1,5 +1,24 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(CircleCollider2D))]
 public class Coin : MonoBehaviour
 {
+    [SerializeField] public int Value {  get; private set; }
+
+    public event Action<Coin, Player> Collected;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out Player player))
+        {
+            Collected?.Invoke(this, player);
+            Destroy(gameObject);
+        }
+    }
+
+    public void Init(int value)
+    {
+        Value = value;
+    }
 }
--- 05.Tasks - 2D platformer/Assets/Scripts/Spawner.cs
+++ 05.Tasks - 2D platformer/Assets/Scripts/Spawner.cs
@@ -1,17 +1,41 @@
 using UnityEngine;
 using UnityEngine.Assertions;
-using UnityEngine.Pool;
+using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private Coin _coin;
+    [SerializeField] private CoinSpawnPoints _coinSpawnPoints;
 
-    private ObjectPool<Coin> _coinPool;
+    private SpawnPoint[] _spawnPoints;
+    private List<Coin> _coins;
 
     private void OnValidate()
     {
         Assert.IsNotNull(_coin);
+        Assert.IsNotNull(_coinSpawnPoints);
     }
 
+    private void Awake()
+    {
+        _spawnPoints = _coinSpawnPoints.GetComponentsInChildren<SpawnPoint>();
+    }
 
+    private void OnEnable()
+    {
+        foreach (SpawnPoint spawnPoint in _spawnPoints)
+        {
+            Coin coin = Instantiate(_coin, spawnPoint.gameObject.transform);
+            _coins.Add(coin);
+            coin.transform.position = spawnPoint.transform.position; 
+        }
+    }
+
+    private void OnDisable()
+    {
+        foreach (Coin coin in _coins)
+        {
+            Destroy(coin.gameObject);
+        }
+    }
 }
