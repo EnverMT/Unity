f22b086609c84b527571019f8b437467f81436c7
EnverMT <anvarmtg@gmail.com>
2024-06-28 16:13:29 +0500

    cleanup

--- 06.UI - Audio Mixer/Assets/Scripts/AudioController.cs
+++ 06.UI - Audio Mixer/Assets/Scripts/AudioController.cs
@@ -6,12 +6,19 @@ public class AudioController : MonoBehaviour
 {
     [SerializeField] private AudioMixerGroup _mixerGroup;
 
+    private const float MaxVolume = 0f;
+    private const float MinVolume = -80f;
+
+    private const float NormalizedMinVolume = 0.0001f;
+    private const float NormalizedMaxVolume = 1f;
+    private const float NormalizedMultiplier = 20f;
+
     public void ToggleMasterAudioMute(bool enabled)
     {
         if (enabled)
-            _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.MasterVolume, 0);
+            _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.MasterVolume, MaxVolume);
         else
-            _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.MasterVolume, -80);
+            _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.MasterVolume, MinVolume);
     }
 
     public void ChangeOverallVolume(float volume)
@@ -31,6 +38,6 @@ public class AudioController : MonoBehaviour
 
     private float GetNormalizedVolume(float volume)
     {
-        return Mathf.Log10(Mathf.Clamp(volume, 0.001f, 1f)) * 20;
+        return Mathf.Log10(Mathf.Clamp(volume, NormalizedMinVolume, NormalizedMaxVolume)) * NormalizedMultiplier;
     }
 }
