01e6ffd1ff65e1325dc548f744f5c9e969af9823
EnverMT <anvarmtg@gmail.com>
2024-06-28 16:10:50 +0500

    v1

--- 06.UI - Audio Mixer/Assets/Scripts/AudioController.cs
+++ 06.UI - Audio Mixer/Assets/Scripts/AudioController.cs
@@ -1,5 +1,36 @@
 using UnityEngine;
+using UnityEngine.Audio;
+
 
 public class AudioController : MonoBehaviour
-{    
+{
+    [SerializeField] private AudioMixerGroup _mixerGroup;
+
+    public void ToggleMasterAudioMute(bool enabled)
+    {
+        if (enabled)
+            _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.MasterVolume, 0);
+        else
+            _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.MasterVolume, -80);
+    }
+
+    public void ChangeOverallVolume(float volume)
+    {
+        _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.OverallVolume, GetNormalizedVolume(volume));
+    }
+
+    public void ChangeButtonVolume(float volume)
+    {
+        _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.ButtonVolume, GetNormalizedVolume(volume));
+    }
+
+    public void ChangeBackgroundMusicVolume(float volume)
+    {
+        _mixerGroup.audioMixer.SetFloat(Params.Audio.Volume.BackgroundVolume, GetNormalizedVolume(volume));
+    }
+
+    private float GetNormalizedVolume(float volume)
+    {
+        return Mathf.Log10(Mathf.Clamp(volume, 0.001f, 1f)) * 20;
+    }
 }
---
+++ 06.UI - Audio Mixer/Assets/Scripts/Params.cs
@@ -0,0 +1,13 @@
+public static class Params
+{
+    public static class Audio
+    {
+        public static class Volume
+        {
+            public const string MasterVolume = nameof(MasterVolume);
+            public const string OverallVolume = nameof(OverallVolume);
+            public const string ButtonVolume = nameof(ButtonVolume);
+            public const string BackgroundVolume = nameof(BackgroundVolume);
+        }
+    }
+}
