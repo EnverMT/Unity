70410375fd506eb339bdef62f2a56bd87cec2bc6
EnverMT <anvarmtg@gmail.com>
2024-07-11 12:51:25 +0500

    v2

--- 06.UI - Health indicator/Assets/Scripts/UI/AttributeIndicator/Health/HealthBarUI.cs
+++ 06.UI - Health indicator/Assets/Scripts/UI/AttributeIndicator/Health/HealthBarUI.cs
@@ -1,10 +1,16 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(Slider))]
 public class HealthBarUI : BaseHealthUI
 {
+    [SerializeField] private bool _smoothSpeed;
+    [SerializeField, Range(0, 1f)] private float _speed;
+
     private Slider _slider;
+    private Coroutine _coroutine;
+    private float _currentValue;
 
     private void Awake()
     {
@@ -13,6 +19,31 @@ public class HealthBarUI : BaseHealthUI
 
     protected override void OnValueChanged(IAttribute<float> attribute)
     {
-        _slider.value = attribute.Value / attribute.MaxValue;
+        if (_coroutine != null)
+            StopCoroutine(_coroutine);
+
+        if (_smoothSpeed)
+        {
+            float target = attribute.Value / attribute.MaxValue;
+            _coroutine = StartCoroutine(SmoothChange(target));
+        }
+        else
+        {
+            _slider.value = attribute.Value / attribute.MaxValue;
+        }
     }
+
+    private IEnumerator SmoothChange(float target)
+    {
+        while (Mathf.Approximately(_currentValue, target) == false)
+        {
+            _currentValue = Mathf.MoveTowards(_currentValue, target, _speed * Time.deltaTime);
+            _slider.value = _currentValue;
+
+            yield return null;
+        }
+
+        _currentValue = target;
+    }
+
 }
