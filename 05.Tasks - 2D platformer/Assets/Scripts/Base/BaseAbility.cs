c6fdff99b296a94fc3dbb9d519d94d60758ea962
EnverMT <anvarmtg@gmail.com>
2024-07-16 12:30:19 +0500

    move

--- 05.Tasks - 2D platformer/Assets/Scripts/Base/BaseAbility.cs
+++ 05.Tasks - 2D platformer/Assets/Scripts/Base/BaseAbility.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 public abstract class BaseAbility : MonoBehaviour
 {
     public abstract KeyCode ActivateKey { get; }
-
-    public abstract bool IsChanneling { get; }
-
-    public abstract float RemainingChannelTime { get; }
-
-    public abstract float RemainingCooldown { get; }
-
     public abstract bool CanBeCasted { get; }
-
+    public abstract bool IsChanneling { get; }
+    public abstract float RemainingChannelTime { get; }
+    public abstract float RemainingCooldown { get; }
     public abstract void Execute(BaseUnit player);
 }
