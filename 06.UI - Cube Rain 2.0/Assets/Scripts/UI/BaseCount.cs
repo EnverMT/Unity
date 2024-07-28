4831ebfa415ff14d860b8ecfe1f6fe183be2354f
EnverMT <anvarmtg@gmail.com>
2024-07-28 17:11:13 +0500

    count text UI

--- 06.UI - Cube Rain 2.0/Assets/Scripts/UI/BaseCount.cs
+++ 06.UI - Cube Rain 2.0/Assets/Scripts/UI/BaseCount.cs
@@ -11,11 +11,11 @@ public class BaseCount : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _currentCount;
     [SerializeField] private TextMeshProUGUI _totalCount;
 
-    private int _activeCubeCount;
-    private int _activeBombCount;
+    private int _activeCubeCount = 0;
+    private int _activeBombCount = 0;
 
-    private int _totalCreatedCubeCount;
-    private int _totalCreatedBombCount;
+    private int _totalCreatedCubeCount = 0;
+    private int _totalCreatedBombCount = 0;
 
     private void OnValidate()
     {
@@ -37,24 +37,43 @@ public class BaseCount : MonoBehaviour
         BombSpawner.Spawned -= OnBombSpawned;
     }
 
+    private void OnGUI()
+    {
+        _currentCount.text = $"Active cubes = {_activeCubeCount}\n";
+        _currentCount.text += $"Active bombs = {_activeBombCount}\n";
+
+        _totalCount.text = $"Total cubes = {_totalCreatedCubeCount}\n";
+        _totalCount.text += $"Total bombs = {_totalCreatedBombCount}\n";
+    }
+
     private void OnCubeSpawned(Cube cube)
     {
         cube.Died += OnCubeDied;
+
+        _activeCubeCount++;
+        _totalCreatedCubeCount++;
     }
 
     private void OnCubeDied(BaseFieldObject cube)
     {
         cube.Died -= OnCubeDied;
+
+        _activeCubeCount--;
     }
 
 
     private void OnBombSpawned(Bomb bomb)
     {
         bomb.Died += OnBombDied;
+
+        _totalCreatedBombCount++;
+        _activeBombCount++;
     }
 
     private void OnBombDied(BaseFieldObject bomb)
     {
         bomb.Died -= OnBombDied;
+
+        _activeBombCount--;
     }
 }
